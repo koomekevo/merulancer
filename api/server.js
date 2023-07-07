7ecb5897367a65625229a9c0271bef41644f06e5
koomekevo <koome.kevo@gurucrafts.net>
2023-07-07 14:07:19 +0300

    Install mongoose

--- api/server.js
+++ api/server.js
@@ -1,1 +1,8 @@
-console.log("test")
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.listen(8800, () => {
+  console.log("Backend server is running");
+});
