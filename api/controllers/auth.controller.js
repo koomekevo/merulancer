d1a2b36fd8d747e56065bc7511700718089d67c0
koomekevo <koome.kevo@gurucrafts.net>
2023-07-07 23:09:02 +0300

    Add login in auth.controller.js

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -15,6 +15,19 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = async (req, res) => {};
+export const login = async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) return res.status(404).send("User not found");
+
+    const isCorrect = bcrypt.compareSync(req.body.password, user.password);
+    if (!isCorrect) return res.status(404).send("Password is incorrect");
+
+    const { password, ...info } = user._doc
+    res.status(200).send(info);
+  } catch (err) {
+    res.status(500).send("Something went wrong");
+  }
+};
 
 export const logout = async (req, res) => {};
