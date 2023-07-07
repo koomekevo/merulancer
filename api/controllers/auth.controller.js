6ee15a7f1ea67e1aee176d7a6c121a7ac3ea9670
koomekevo <koome.kevo@gurucrafts.net>
2023-07-07 23:28:41 +0300

    Add jwt to auth.controller.js

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
   try {
@@ -23,8 +24,20 @@ export const login = async (req, res) => {
     const isCorrect = bcrypt.compareSync(req.body.password, user.password);
     if (!isCorrect) return res.status(404).send("Password is incorrect");
 
-    const { password, ...info } = user._doc
-    res.status(200).send(info);
+    const token = jwt.sign(
+      {
+        id: user._id,
+        isSeller: user.isSeller,
+      },
+      process.env.JWT_KEY
+    );
+
+    const { password, ...info } = user._doc;
+    res
+      .cookie("accessToken", token, {
+        httpOnly: true,
+      })
+      .send(info);
   } catch (err) {
     res.status(500).send("Something went wrong");
   }
