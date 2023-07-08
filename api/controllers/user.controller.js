59c01f2c57dc5dafdcc91c14b653a935bf1b5034
koomekevo <koome.kevo@gurucrafts.net>
2023-07-08 09:03:44 +0300

    Modify user.controller.js

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,17 +1,11 @@
 import User from "../models/user.model.js";
-import createError from "../utils/createError.js";
+import jwt from "jsonwebtoken";
 
-export const deleteUser = async (req, res, next) => {
-  const user = await User.findById(req.params.id);
+export const deleteUser = async (req, res) => {
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).send("You are not authenticated");
 
-  if (req.userId !== user._id.toString()) {
-    return next(createError(403, "You can delete only your account!"));
-  }
-  await User.findByIdAndDelete(req.params.id);
-  res.status(200).send("deleted.");
-};
-export const getUser = async (req, res, next) => {
-  const user = await User.findById(req.params.id);
-
-  res.status(200).send(user);
+  jwt.verify(token, process.env.JWT_KEY, (err, payload) => {
+    res.send(payload);
+  });
 };
